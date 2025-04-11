841e8a71ac261853f20acf28ad1241774c607ebb
Valentin S. <akirpap03@gmail.com>
2025-04-11 13:49:28 +0200

    Aufgabe 1 b)

--- 1Uebung/Main.c
+++ 1Uebung/Main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <time.h>
 
-int main() {
+void print_timeB() {
+  time_t current_time;
+  struct tm *local_time;
+  char buffer[80];
+  current_time = time(NULL);
+  local_time = localtime(&current_time);
+  strftime(buffer, sizeof(buffer),  "%a %b %d %H:%M:%S %Z %Y", local_time);
+  printf("%s", buffer);
+}
+
+void print_timeA() {
   time_t current_time;
 
   current_time = time(NULL);
@@ -9,6 +19,9 @@ int main() {
   char *time_string = ctime(&current_time);
 
   printf("%s", time_string);
-
+}
+int main() {
+  print_timeA();
+  print_timeB();
   return 0;
 }
