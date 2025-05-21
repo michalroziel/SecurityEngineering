fcd0a208376e4b9bc0798078976905c39a919c14
Valentin S. <akirpap03@gmail.com>
2025-05-21 09:14:27 +0200

    viewer

--- 3Uebung/viewer.sh
+++ 3Uebung/viewer.sh
@@ -1,10 +1,23 @@
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 <filename>"
-  exit 1
+#!/bin/sh
+
+# Check Fehlerhafte Eingabe
+if [ "$#" -eq 0 ]; then
+    echo "Fehler: Keine Datei angegeben."
+    exit 1
+elif [ "$#" -gt 1 ]; then
+    echo "Fehler: Bitte nur eine Datei angeben."
+    echo "Usage: $0 <filename>"
+    exit 1
 fi
 
-
 filename="$1"
+
+# Prüfen, ob die Datei existiert und lesbar ist
+if [ ! -r "$filename" ]; then
+    echo "Fehler: Datei '$filename' existiert nicht oder ist nicht lesbar."
+    exit 1
+fi
+
 filetype="$(file "$filename")"
 
 ## Unterscheiden :
@@ -13,7 +26,6 @@ filetype="$(file "$filename")"
 ## PDF-Dateien : xpdf
 ## Textdateien : less
 ## Open-Document Dateien : libre office
-
 
 
 if echo "$filetype" | grep -q ".txt"; then
