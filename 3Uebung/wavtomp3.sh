e880daf52dceb2f2d67f77a018204cf4e804175b
Michał Roziel <miro00009@htwsaar.de>
2025-05-21 22:35:43 +0200

    update

--- 3Uebung/wavtomp3.sh
+++ 3Uebung/wavtomp3.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
+# Wir benutzen ffmpeg, einen universellen media converter
 
-# Überprüfen, ob ffmpeg installiert ist
-if ! command -v ffmpeg >/dev/null 2>&1; then
-    echo "Fehler: ffmpeg ist nicht installiert oder nicht im Pfad."
-    exit 1
-fi
-
-# Überprüfen, ob genau eine Datei angegeben wurde
+# Überprüfen, ob genau eine Datei angegeben wurde (Anzahl ist 1)
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <datei.wav>"
+    echo "Usage: $0 < myFile.wav >"
     exit 1
 fi
 
+# Unsere Datei als erstes Argument setzen
 DATEI="$1"
 
-# Prüfen, ob Datei existiert
+# Prüfen, ob Datei existiert und "block special file " ist
+# -> man test
+
 if [ ! -f "$DATEI" ]; then
     echo "Fehler: Datei '$DATEI' existiert nicht."
     exit 1
 fi
 
 # Prüfen, ob die Datei eine WAV-Datei ist
+# parameter expansion
+#https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
 EXT="${DATEI##*.}"
-if [ "$EXT" != "wav" ] && [ "$EXT" != "WAV" ]; then
+if [ "$EXT" != "wav" ]; then
     echo "Fehler: '$DATEI' ist keine WAV-Datei."
     exit 1
 fi
