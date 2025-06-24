0b08a9e453b8c6c1e6b33c45d0c240b4ea4e4ec4
Michał Roziel <miro00009@htwsaar.de>
2025-06-24 22:36:58 +0200

    update

--- 5Uebung/limits.sh
+++ 5Uebung/limits.sh
@@ -4,10 +4,9 @@ echo -e "Welcome to the Limit checker !"
 
 ## Setting the limits
 
-ulimit -f 1000000
-
-## the limits mean : 
-# -f 1000000 : maximum file size is 1GB
+ulimit -s 16384   ## maximum stack size is 16MB
+ulimit -t 30      ## maximum CPU time is 30 seconds
+ulimit -f 1000000 ## maximum file size is 1GB
 
 echo -e "Limits set successfully !"
 
