9969e4df4165b479c5dcc74349f4fb785b186088
Michał Roziel <miro00009@htwsaar.de>
2025-06-25 15:38:43 +0200

    created signal handlers, and limits for cpu time, stack size and file size

--- 5Uebung/limits.sh
+++ 5Uebung/limits.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-echo -e "Welcome to the Limit checker !"
+    echo "== Testing CPU limit =="
+    (
+        ulimit -t 1
+        ## create signal handler to catch cpu limit
+        ## after limit is exceeded
+        trap 'echo "CPU limit exceeded!"; exit 0' SIGXCPU
+        echo "Testing CPU limit..."
+        while true; do
+            : # Infinite loop to consume CPU
+        done
+    )
 
-## Setting the limits
+    echo -e "\n== Testing stack limit =="
+    (
+        ulimit -s 3
+        echo "Testing stack limit..."
+        ## Simple recursive function with no trap (system will show segfault)
+        ## function calls itself indefinitely w/o a base case
+        ## seg fault -> stack limit is being enforced correctly
+        ## no signal handler as in case of seg fault,
+        ## programme execution is terminated directly
 
-ulimit -s 16384   ## maximum stack size is 16MB
-ulimit -t 30      ## maximum CPU time is 30 seconds
-ulimit -f 1000000 ## maximum file size is 1GB
+        f() { f; }; f
+    )
 
-echo -e "Limits set successfully !"
-
-
+    echo -e "\n== Testing file size limit =="
+    (
+        ulimit -f 5
+        trap 'echo "File size limit exceeded!"; exit 0' SIGXFSZ
+        echo "Testing file size limit..."
+        dd if=/dev/zero of=testfile bs=1024 count=1024 2>/dev/null
+        rm -f testfile
+    )
