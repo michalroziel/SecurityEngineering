3fae4fdeb413498e70363ec1e3bc5956b33dd3c6
Michał Roziel <miro00009@htwsaar.de>
2025-06-25 16:24:56 +0200

    added comments

--- 5Uebung/limits.sh
+++ 5Uebung/limits.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+export CPU_TIME_LIMIT=1     ## 1 Second
+export STACK_SIZE_LIMIT=3   ## 3 Kilobyte
+export FILE_SIZE_LIMIT=5    ## 5 Kilobyte
 
     echo "== Testing CPU limit =="
-    (
-        ulimit -t 1
+    (   ## start subshell
+        ulimit -t $CPU_TIME_LIMIT
         ## create signal handler to catch cpu limit
-        ## after limit is exceeded
+        ## after limit is exceeded system sends SIGXCPU
+        ## to process which triggers the sig handler
         trap 'echo "CPU limit exceeded!"; exit 0' SIGXCPU
         echo "Testing CPU limit..."
         while true; do
@@ -13,22 +17,35 @@
     )
 
     echo -e "\n== Testing stack limit =="
-    (
-        ulimit -s 3
+    (   ## start subshell
+        ulimit -s $STACK_SIZE_LIMIT
         echo "Testing stack limit..."
         ## Simple recursive function with no trap (system will show segfault)
         ## function calls itself indefinitely w/o a base case
         ## seg fault -> stack limit is being enforced correctly
+
         ## no signal handler as in case of seg fault,
+        ## SIGSEGV signal
         ## programme execution is terminated directly
+        ## and after seg fault programme is in invalid state
 
         f() { f; }; f
     )
 
     echo -e "\n== Testing file size limit =="
     (
-        ulimit -f 5
+        ulimit -f $FILE_SIZE_LIMIT
+        ## exit 0 stops execution of the subshell
         trap 'echo "File size limit exceeded!"; exit 0' SIGXFSZ
+
+        ## dd used to convert data
+        ## dd receives signal 25 (SIGXFSZ) when beyond 5KB
+        ## input source is /dev/zero
+        ## of - output file is "testfile"
+        ## bs - block size of 1KB read/write
+        ## count - copy 1024 bytes in total
+        ## redirect error message :2>/dev/null
+
         echo "Testing file size limit..."
         dd if=/dev/zero of=testfile bs=1024 count=1024 2>/dev/null
         rm -f testfile
